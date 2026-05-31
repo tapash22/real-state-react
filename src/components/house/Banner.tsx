079bd60772a02d24108f87cc0f31947b20530b2c
tapash22 <tapasp263@gmail.com>
2026-05-31 16:28:35 +0600

    update with some text changes

--- src/components/house/Banner.tsx
+++ src/components/house/Banner.tsx
@@ -13,15 +13,14 @@ export function Banner(_props: BannerProps) {
         <div className="bg-white p-10 block mx-auto">
           <div className="flex justify-start mb-4">
             <span className="text-3xl font-bold uppercase mx-4 text-start">
-              Rent Your Dream House With Us
+              Your Home Anywhere. Stay for Days, Months, or Longer.
             </span>
           </div>
 
           <div className="flex">
             <p className="text-justify text-sm mx-4 font-semibold">
-              Find your perfect home with ease. We help you discover verified
-              rental properties that match your lifestyle, budget, and
-              preferences with full trust and transparency.
+              Connect directly with local homeowners across the globe. Secure
+              your space, negotiate your terms, and live like a local.
             </p>
           </div>
         </div>
