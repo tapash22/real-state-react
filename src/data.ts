648b195c3603d1f9148588086b0d3396d32ec741
tapash22 <tapasp263@gmail.com>
2026-07-03 19:00:51 +0600

    update

--- src/data.ts
+++ src/data.ts
@@ -20,33 +20,6 @@ export interface SocialMediaItem {
 
 export type SocialMediaItems = SocialMediaItem[];
 
-export const socialMediaLinkList: SocialMediaItems = [
-  {
-    id: 1,
-    title: "Facebook",
-    link: "https://web.facebook.com/tapas.paul.54738/",
-    icon: FaFacebook,
-  },
-  {
-    id: 2,
-    title: "Twitter",
-    link: "https://x.com/tapasp263",
-    icon: FaTwitter,
-  },
-  {
-    id: 3,
-    title: "GitHub",
-    link: "https://github.com/tapash22",
-    icon: FaGithub,
-  },
-  {
-    id: 4,
-    title: "LinkedIn",
-    link: "https://www.linkedin.com/in/tapash-paul-267896151/",
-    icon: FaLinkedin,
-  },
-];
-
 export interface RentStepData {
   id: number;
   title: string;
@@ -130,6 +103,13 @@ export interface Place {
   agencies: Agency[];
 }
 
+export interface FaqItem {
+  id: number;
+  question: string;
+  answer: string;
+  link?: string | null;
+}
+
 // export interface House {
 //   id: number;
 //   type: string;
@@ -148,6 +128,33 @@ export interface Place {
 //   price: string;
 //   agent: Agent;
 // }
+
+export const socialMediaLinkList: SocialMediaItems = [
+  {
+    id: 1,
+    title: "Facebook",
+    link: "https://web.facebook.com/tapas.paul.54738/",
+    icon: FaFacebook,
+  },
+  {
+    id: 2,
+    title: "Twitter",
+    link: "https://x.com/tapasp263",
+    icon: FaTwitter,
+  },
+  {
+    id: 3,
+    title: "GitHub",
+    link: "https://github.com/tapash22",
+    icon: FaGithub,
+  },
+  {
+    id: 4,
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/tapash-paul-267896151/",
+    icon: FaLinkedin,
+  },
+];
 
 export interface House {
   id: number;
@@ -309,13 +316,6 @@ export const houseData: House[] = [
 ];
 
 // Q & A
-
-export interface FaqItem {
-  id: number;
-  question: string;
-  answer: string;
-  link?: string | null;
-}
 
 export const faqData: FaqItem[] = [
   {
